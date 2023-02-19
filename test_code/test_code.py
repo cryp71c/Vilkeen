6400242ccaeacfd5f36917876270e044e2f56ef5
Alex <alexrapino75@gmail.com>
2023-02-19 00:20:37 -0600

    Some test code added to test_code.py

--- test_code/test_code.py
+++ test_code/test_code.py
@@ -1,13 +1,15 @@
 """
  Every 10 levels your exp needed for the next level will be 1000*level
+
+level = 2
+if (level%10)+1 == 0:
+    exp_needed = level*1000
+    print(exp_needed)
+else:
+    exp_needed = level*1000
+    print(exp_needed)
+
 """
-# level = 2
-# if (level%10)+1 == 0:
-#     exp_needed = level*1000
-#     print(exp_needed)
-# else:
-#     exp_needed = level*1000
-#     print(exp_needed)
 
 item = {"drop_chance": 0.00025}
 times_killed = 100
@@ -15,6 +17,26 @@ base_drop_chance = item['drop_chance']
 base_exp_chance = 3
 player_level = 25
 enemy_level = 3
+
+player = {
+    "name": "Emily",
+    "class": "Warlock",
+    "status": "Middle Class",
+    "exp": 400,
+    "level": 1,
+}
+
+
+def level_updater(exp, level):
+    pass
+    """
+        if exp <= (level*1000):
+            return(exp, level)
+    """
+
+
+def update_player(t_exp, ):
+    pass
 
 
 def exp_calc(bec, el, pl):
@@ -27,8 +49,9 @@ def exp_calc(bec, el, pl):
 
 def drop_calc_enemy(bdc, el, pl, tk, t_exp):
     dc = (0.0005 * pl + bdc * el + t_exp * (tk * 0.01)) - 1
-    print(dc)
+    print("Drop Chance: ", dc)
     return dc
 
 
-drop_chance = drop_calc_enemy(base_drop_chance, enemy_level, player_level, times_killed, exp_calc(base_exp_chance, enemy_level, player_level))
+drop_chance = drop_calc_enemy(base_drop_chance, enemy_level, player_level, times_killed,
+                              exp_calc(base_exp_chance, enemy_level, player_level))
