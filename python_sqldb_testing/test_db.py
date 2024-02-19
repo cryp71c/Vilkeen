0a7d0068630a98587f1e5f02c5c3c634a4c0fbf9
Alex <alexrapino75@gmail.com>
2024-02-18 22:27:57 -0600

    Creating sql tables, and linking the databases to gether, neeed to create, player inventory

--- python_sqldb_testing/test_db.py
+++ python_sqldb_testing/test_db.py
@@ -1,26 +1,25 @@
 import os
+import pandas as pd
 import sqlite3
+from IPython.display import display
 
 main_dir = os.path.dirname(os.path.dirname(os.path.realpath(__name__)))
 database_loc = main_dir+"/data_bases/vilkeen.db"
 
 main_db = sqlite3.connect(database_loc)
-cur = main_db.cursor()
+#cur = main_db.cursor()
 
 # TEST
 
-if(int(cur.execute("SELECT COUNT(*) FROM SAVES").fetchall()[0][0])==0):
+main_df = pd.read_sql_query("SELECT * FROM saves", main_db)
 
+display(main_df)
 
 ## END TEST
 
 def new_save(save_id, name):
-
+    pass
 
 
 def test_main():
-    if (int(cur.execute("SELECT COUNT(*) FROM SAVES").fetchall()[0][0]) == 0):
-        save_id = 1
-    else:
-        save_id = int(cur.execute("SELECT save_id FROM saves ORDER BY save_id DESC LIMIT 1")) + 1
-    name = input()
+    pass
